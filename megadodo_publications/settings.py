3522cde255c94c2c39d66eeac4e6eaa0b6228f32
mrsmlisa <lisa.q.scott@gmail.com>
2026-06-15 14:59:15 +0200

    Add allaut to project

--- megadodo_publications/settings.py
+++ megadodo_publications/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'megadodo_publication.urls'
+ROOT_URLCONF = 'megadodo_publications.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +72,19 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'megadodo_publication.wsgi.application'
+AUTHENTICATION_BACKENDS = [
+
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend',
+
+]
+
+SITE_ID = 1
+
+WSGI_APPLICATION = 'megadodo_publications.wsgi.application'
 
 
 # Database
--- megadodo_publications/wsgi.py
+++ megadodo_publications/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'megadodo_publication.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'megadodo_publications.settings')
 
 application = get_wsgi_application()
