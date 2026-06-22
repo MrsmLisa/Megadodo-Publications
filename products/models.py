552971d00ac19afde0b6c467d2bdc1570362f5a2
mrsmlisa <lisa.q.scott@gmail.com>
2026-06-22 10:35:25 +0200

    Made products app. Migrated but put it in models in home not in products. Changed and migrated again

---
+++ products/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class books(models.Model):
+     title = models.CharField(max_length=200)
+     author = models.CharField(max_length=100)
+     description = models.TextField()
+     price = models.DecimalField(max_digits=6, decimal_places=2)
+     cover_image = models.ImageField(upload_to='book_covers/')
+     category = models.CharField(max_length=100)
+     sku = models.CharField(max_length=50, unique=True)
