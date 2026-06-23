4ec4da26b6dd3e33ad29159d5e330bb6b18d1e61
mrsmlisa <lisa.q.scott@gmail.com>
2026-06-23 12:22:17 +0200

    Add images for book-covers

--- products/views.py
+++ products/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-from .models import books
+from .models import Book
+from django.views.generic import ListView
 
 # Create your views here.
 
 def book_list(request):
-    books = books.objects.all()
+    books = Book.objects.all()
     return render(request, 'products/book_list.html', {'books': books})
 
 class BookListView(ListView):
-    model = books
+    model = Book
     template_name = 'products/book_list.html'
     context_object_name = 'books'
     paginate_by = 4
