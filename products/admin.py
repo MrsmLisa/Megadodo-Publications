145079bf334d99cb8ef8eb24fd0db77e87c6ba72
mrsmlisa <lisa.q.scott@gmail.com>
2026-06-23 09:28:59 +0200

    Made products.html and connected everything.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Book 
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'price', 'category', 'sku')
+    search_fields = ('title', 'author', 'category', 'sku')
+    list_filter = ('category',)
+
+
+admin.site.register(Book, BookAdmin)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import books
 
 # Create your views here.
+
+def book_list(request):
+    books = books.objects.all()
+    return render(request, 'products/book_list.html', {'books': books})
+
+class BookListView(ListView):
+    model = books
+    template_name = 'products/book_list.html'
+    context_object_name = 'books'
+    paginate_by = 4
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category = self.request.GET.get('category')
+        if category:
+            queryset = queryset.filter(category=category)
+        return queryset
