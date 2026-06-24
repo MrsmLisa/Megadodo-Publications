1bcda99c435b9b31345f73f5b589647e9ceba66e
mrsmlisa <lisa.q.scott@gmail.com>
2026-06-24 16:02:15 +0200

    Adding images and text for the index and installed summernote

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Product, Category 
 
 # Register your models here.
 
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description',)
     list_display = ('name', 'author', 'price', 'category', 'sku')
     search_fields = ('name', 'author', 'category', 'sku')
     list_filter = ('category',)
