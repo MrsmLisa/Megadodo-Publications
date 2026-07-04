39592f39b540a60fbc32ff759fd8255ceb46be60
mrsmlisa <lisa.q.scott@gmail.com>
2026-07-04 11:16:27 +0200

    Trying to se where the problem is with heroku

--- megadodo_publications/settings.py
+++ megadodo_publications/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-5mx)6j8mbf+3ifcmia_dqyn8dpnkm#xx#hwb2bc^&opcilz33)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
