8662ded385c40e7835fcb5119426f11fe73ed662
mrsmlisa <lisa.q.scott@gmail.com>
2026-07-04 11:09:05 +0200

    Added CSRF

--- megadodo_publications/settings.py
+++ megadodo_publications/settings.py
@@ -35,6 +35,11 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://megadodo-publications-8e99ae98d78e.herokuapp.com',
+    'https://*.herokuapp.com',
+]
+
 
 # Application definition
 
