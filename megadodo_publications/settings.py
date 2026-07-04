7d45dc511c494aa2880fef87e28f75090089db0b
mrsmlisa <lisa.q.scott@gmail.com>
2026-07-04 10:40:49 +0200

    Fixed allowed hosts and debug setting

--- megadodo_publications/settings.py
+++ megadodo_publications/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-5mx)6j8mbf+3ifcmia_dqyn8dpnkm#xx#hwb2bc^&opcilz33)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'herokuapp.com',
