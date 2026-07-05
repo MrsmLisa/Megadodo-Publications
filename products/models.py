8eadbdf4d4256450b614a78aaf8075dc1f304c19
mrsmlisa <lisa.q.scott@gmail.com>
2026-07-05 11:28:44 +0200

    Small changes to make the webhook work better

--- products/models.py
+++ products/models.py
@@ -12,13 +12,13 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
-     name = models.CharField(max_length=200)
-     author = models.CharField(max_length=100, null=True, blank=True)
-     description = models.TextField()
-     price = models.DecimalField(max_digits=6, decimal_places=2)
-     image = models.ImageField(upload_to='products/', null=True, blank=True)
-     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='books')
-     sku = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='books')
+    sku = models.CharField(max_length=50, unique=True)
 
-     def product_url(self):
-         return reverse('product_detail', kwargs={'pk': str(self.id)})
+    def product_url(self):
+        return reverse('product_detail', kwargs={'pk': str(self.id)})
