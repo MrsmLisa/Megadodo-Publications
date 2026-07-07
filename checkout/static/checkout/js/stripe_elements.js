81970a21351402da8b0d1c962de7ba573408ce49
mrsmlisa <lisa.q.scott@gmail.com>
2026-07-07 15:01:16 +0200

    Added style changes to the different html after changing bg-color

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -76,8 +76,8 @@ form.addEventListener('submit', function(ev) {
                         line2: $.trim(form.street_address2.value),
                         city: $.trim(form.town_or_city.value),
                         country: $.trim(form.country.value),
-                        state: $.trim(form.county.value),
                     }
+                }    
             }
         }).then(function(result) {
             if (result.error) {
