1c22dfa43acd941103594c6d49c64bd469dfd0a4
mrsmlisa <lisa.q.scott@gmail.com>
2026-07-08 14:47:59 +0200

    realized my djangokey was in settings, took it away and made a new key

--- megadodo_publications/settings.py
+++ megadodo_publications/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-5mx)6j8mbf+3ifcmia_dqyn8dpnkm#xx#hwb2bc^&opcilz33)')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEVELOPMENT') == 'True'
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
