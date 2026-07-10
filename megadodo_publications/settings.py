ffa424e51f3e0b712d434a427d3e3b50b76b4b82
mrsmlisa <lisa.q.scott@gmail.com>
2026-07-10 12:05:22 +0200

    use whitenoise for static files storage

--- megadodo_publications/settings.py
+++ megadodo_publications/settings.py
@@ -195,11 +195,11 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
