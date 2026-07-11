3b7c30d1b1bc7aa5ab456ca9b94fc58260187695
mrsmlisa <lisa.q.scott@gmail.com>
2026-07-11 19:31:31 +0200

    Add models, urls and views to forum

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Question(models.Model):
+    question = models.CharField(max_length=300)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='questions')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question
+    
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    answer = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='answers')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Answer to: {self.question.question}'  # Display the first 50 characters of the answer
