3cb87e57a27ef7f37a19a01975aa7d6b3cbe8f3c
mrsmlisa <lisa.q.scott@gmail.com>
2026-07-11 17:15:19 +0200

    Add a faq app

---
+++ faq/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Faq(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question
+    
+    class Meta:
+        verbose_name = "FAQ"
+        verbose_name_plural = "FAQs"
+        ordering = ['created_at']
