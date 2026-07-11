43ca8368ea71b3625fc8a8b8932375c47d63362e
mrsmlisa <lisa.q.scott@gmail.com>
2026-07-11 17:05:54 +0200

    Change the contact form

--- contact/urls.py
+++ contact/urls.py
@@ -3,7 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.contact_create, name='contact'),
-    path('list/', views.contact_read, name='contact_read'),
-    path('update/<int:contact_id>/', views.contact_update, name='contact_update'),
-    path('delete/<int:contact_id>/', views.contact_delete, name='contact_delete'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Contact
-from django.contrib.auth.decorators import login_required
 from .forms import ContactForm
 
 # Create your views here.
@@ -25,29 +24,3 @@ def contact_read(request):
     from .models import Contact
     contacts = Contact.objects.all()
     return render(request, 'contact/contact_list.html', {'contacts': contacts})
-
-@login_required
-def contact_update(request, contact_id):
-    from .models import Contact
-    contact = get_object_or_404(Contact, id=contact_id)
-
-    if request.method == 'POST':
-        contact.name = request.POST.get('name')
-        contact.email = request.POST.get('email')
-        contact.subject = request.POST.get('subject')
-        contact.message = request.POST.get('message')
-        contact.save()
-
-        return render(request, 'contact/thank_you.html', {'name': contact.name})
-
-    form = ContactForm(instance=contact)
-    return render(request, 'contact/contact_form.html', {'form': form, 'contact': contact})
-
-
-@login_required
-def contact_delete(request, contact_id):
-    from .models import Contact
-    contact = get_object_or_404(Contact, id=contact_id)
-    contact.delete()
-    return redirect('contact_read')  # Redirect to the contact list view after deletion
-
