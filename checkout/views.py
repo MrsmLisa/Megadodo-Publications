bbb8ceb5b44735ba1ed5060758a16a61333e07db
mrsmlisa <lisa.q.scott@gmail.com>
2026-07-11 10:30:13 +0200

    Change the success message for checkout.

--- checkout/views.py
+++ checkout/views.py
@@ -152,8 +152,8 @@ def checkout_success(request, order_number):
                 user_profile_form.save()
 
     messages.success(request, f'Order successfully processed! \
-        Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
+        Your order number is {order_number}. \
+        You can view your order history in your profile.')
 
     if 'bag' in request.session:
         del request.session['bag']
