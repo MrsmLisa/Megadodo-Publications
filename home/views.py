dc8c9cc30d1409dcfa87d79cb33226235e0b5366
mrsmlisa <lisa.q.scott@gmail.com>
2026-07-11 13:00:41 +0200

    Point the cards to the book, not the shop

--- home/views.py
+++ home/views.py
@@ -15,3 +15,16 @@ def terms (request):
 
 def delivery (request):
     return render(request, 'home/delivery.html')
+
+def index(request):
+    try:
+        hitchhikers = Product.objects.get(name='The Hitchhiker\'s Guide to the Galaxy')
+        fifty_threes = Product.objects.get(name='53 More Things to Do in Zero Gravity')
+    except Product.DoesNotExist:
+        hitchhikers = None
+        fifty_threes = None
+
+    return render(request, 'home/index.html', {
+        'hitchhikers': hitchhikers, 
+        'fifty_threes': fifty_threes,
+    })
